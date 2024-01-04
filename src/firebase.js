a9c5c41fb9c6cd91ae01784683915d8c892fa5dc
ohzuma <ozumanology@gmail.com>
2024-01-04 14:49:10 -0800

    env

--- src/firebase.js
+++ src/firebase.js
@@ -5,17 +5,16 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "process.env.REACT_APP_API_FIREBASE_KEY",
-  authDomain: "process.env.REACT_APP_FIREBASE_AUTH_DOMAIN",
-  projectId: "process.env.REACT_APP_FIREBASE_PROJECT_ID",
-  storageBucket: "process.env.REACT_APP_FIREBASE_STORAGE_BUCKET",
-  messagingSenderId: "process.env.REACT_APP_FIREBASE_MESSAGING_SENDER",
-  appId: "process.env.REACT_APP_APP_ID",
-  measurementId: "process.env.REACT_APP_MEASUREID",
-  databaseURL: "process.env.REACT_APP_DATABASE_URL",
+  apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREID,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
 };
 
-// Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 // const analytics = getAnalytics(app);
 export const auth = getAuth(app);
